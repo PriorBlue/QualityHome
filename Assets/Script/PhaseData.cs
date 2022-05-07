e86cf6a6eabea55ff0b0ea3b567fbe3a611ad0c5
Marcus <marcusplate@googlemail.com>
2022-05-07 19:08:23 +0200

    Improve phases.

--- Assets/Script/PhaseData.cs
+++ Assets/Script/PhaseData.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "QualityHome/Phase")]
 public class PhaseData : ScriptableObject
 {
+    [Header("Info")]
     public string Name;
+    public string Title;
+    [Multiline]
+    public string Description;
+
+    [Header("Settings")]
+    public float Delay = 1f;
+
     [Header("Categories")]
     public List<CategoryInfo> Categories = new List<CategoryInfo>();
 
