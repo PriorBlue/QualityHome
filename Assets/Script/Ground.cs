666346c81ace4f1dbdbd235938532989bd3377b7
Marcus <marcusplate@googlemail.com>
2022-05-07 11:51:07 +0200

    Add first scene.

---
+++ Assets/Script/Ground.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public BoxCollider2D Box;
+    public RectTransform Rect;
+
+    private void OnValidate()
+    {
+        Fit();
+    }
+
+    private void Reset()
+    {
+        Box = GetComponent<BoxCollider2D>();
+        Rect = GetComponent<RectTransform>();
+
+        Fit();
+    }
+
+    private void Awake()
+    {
+        Fit();
+    }
+
+    private void Fit()
+    {
+        Box.size = new Vector2(
+            Screen.width,
+            Rect.sizeDelta.y
+        );
+
+        Box.offset = new Vector2(
+            0f,
+            Rect.sizeDelta.y * 0.5f
+        );
+    }
+}
