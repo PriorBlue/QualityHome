fa69c75446372c98ad971fd21deb4739083397ea
Marcus <marcusplate@googlemail.com>
2022-05-07 12:31:11 +0200

    Add Spawner.

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("References")]
+    public Transform Canvas;
+
+    [Header("Settings")]
+    public Vector2 Size;
+    public float Delay = 5f;
+
+    [Header("Tiles")]
+    public List<GameObject> Tiles = new List<GameObject>();
+
+    private GameObject randTile;
+    private Vector3 randPosition;
+    private float lastSpawn;
+
+    private void Start()
+    {
+        lastSpawn = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= lastSpawn + Delay)
+        {
+            lastSpawn = Time.time;
+
+            randTile = Tiles[Random.Range(0, Tiles.Count)];
+            randPosition = new Vector3(Random.Range(-Size.x, Size.x), Random.Range(-Size.y, Size.y), 0f);
+
+            var tile = Instantiate(randTile, transform.position + randPosition, Quaternion.identity, Canvas);
+
+            tile.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-20f, 20f), 100f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, Size * 2f);
+    }
+}
---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    [Header("References")]
+    public Image Image;
+
+    [Header("Settings")]
+    public float FadeIn = 1f;
+
+    private float spawnTime;
+    private float currFade;
+
+    private void Start()
+    {
+        spawnTime = Time.time;
+
+        Image.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    private void Update()
+    {
+        currFade = (Time.time - spawnTime) / FadeIn;
+
+        if (currFade < 1f)
+        {
+            Image.color = new Color(1f, 1f, 1f, currFade);
+        }
+        else
+        {
+            Image.color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}
